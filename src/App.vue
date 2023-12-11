8ab23c2f9e7d78d702b492ea05d5d39036286072
chianyou <91866952+chianyou@users.noreply.github.com>
2023-12-11 15:55:05 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,46 @@
 <template>
-  <CoinFlip ></CoinFlip>
-
+  <div>
+    <WalletButton msg="The Multi Button"/>
+    <CoinGame msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-import CoinFlip from './components/CoinFlip.vue'
+import WalletButton from './components/WalletButton.vue';
+import CoinGame from './components/CoinGame.vue'
+import {
+  PhantomWalletAdapter
+} from '@solana/wallet-adapter-wallets';
+import { initWallet } from 'solana-wallets-vue';
 
 export default {
   name: 'App',
   components: {
-    CoinFlip
+    WalletButton,
+    CoinGame
+  },
+  setup() {
+    console.log('Initializing wallet...');
+
+    const walletOptions = {
+      wallets: [
+        new PhantomWalletAdapter()
+      ],
+      autoConnect: true,
+    };
+
+    initWallet(walletOptions);
   }
 }
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
